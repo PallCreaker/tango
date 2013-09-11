105ad8eec937a8e776fc59aabd37e7b1baf20514
Takumi Takahashi <kururu1018@gmail.com>
2013-09-11 12:57:45 +0900

    Profileのeditメソッド実装

--- app/Controller/ProfilesController.php
+++ app/Controller/ProfilesController.php
@@ -19,16 +19,26 @@ class ProfilesController extends AppController {
     
     public function edit ($user_id = null) {
         $this->Profile->user_id = $user_id;
-        if ($this->request->is('post') && $this->Profile->exists()) {
-            if ($this->Profile->save(json_decode($this->request->data))) {
-                echo 'プロフィールが正しく更新されました!';
-                $profile = $this->Profile->get_user_profile($user_id);
-                $this->set('profile', $profile);
-            }
-        } else {
-            $profile = array('hoge' => 'hoge');
-            $this->set('profile', json_encode($profile));
-            echo '指定されたユーザーは存在しません。';
+        
+        if($this->request->is('post')){
+            $this->Profile->save($this->request->data);
         }
+        
+        $profile = $this->Profile->get_user_profile($user_id);
+        $this->set('profile', $profile);
+        
+      //  $this->Profile->save(json_decode($this->request->data));
+      //  debug($this->request->data);
+//        if ($this->request->is('post') && $this->Profile->exists()) {
+//            if ($this->Profile->save(json_decode($this->request->data))) {
+//                echo 'プロフィールが正しく更新されました!';
+//                $profile = $this->Profile->get_user_profile($user_id);
+//                $this->set('profile', $profile);
+//            }
+//        } else {
+//            $profile = array('hoge' => 'hoge');
+//            $this->set('profile', json_encode($profile));
+//            echo '指定されたユーザーは存在しません。';
+//        }
     }
 }
--- app/Model/Profile.php
+++ app/Model/Profile.php
@@ -4,9 +4,9 @@ class Profile extends AppModel {
     public $name = 'Profile';
     
     public function get_user_profile($user_id = null) {
-        $this->Profile->user_id = $user_id;
+      //  $this->Profile->user_id = $user_id;
         $profile = $this->find('first', array(
-            'conditions' => array('User.id' => $user_id),
+            'conditions' => array('Profile.user_id' => $user_id),
             'fields' => array('user_id', 'color_id', 'message'),
             'recursive' => -1
         ));
